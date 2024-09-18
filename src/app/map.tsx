4cf26e7c4959e444fa4b8759fe8dfaca724fde59
Baerael <badartic@yahoo.com>
2024-09-18 14:37:17 -0600

    marker info window

--- src/app/map.tsx
+++ src/app/map.tsx
@@ -1,22 +1,44 @@
 "use client";
-import { Flex } from '@radix-ui/themes';
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import { useState } from 'react';
+import { Flex, Box } from '@radix-ui/themes';
+import { APIProvider, Map, Marker, InfoWindow } from '@vis.gl/react-google-maps';
+import routeData from './data.json'
 
-const API_KEY = ""
+interface MarkerData {
+    latitude: number;
+    longitude: number;
+    name: string;
+  }
+
+const API_KEY = "AIzaSyACszpFGn8QLyvXcIlmDU3rVhrrHfdzj0Y"
 
 export default function GoogleMaps() {
+    const [selectedMarker, setSelectedMarker] = useState<MarkerData | null>(null);
+
   return (
     <div>
         <Flex justify="center" gap="2">
             <APIProvider apiKey={API_KEY}>
                 <Map
                 style={{width: '50vw', height: '50vh'}}
-                defaultCenter={{lat: 40.000614, lng: -105.306558}}
+                defaultCenter={{lat: 40.001424, lng: -105.296429}}
                 defaultZoom={15}
                 gestureHandling={'greedy'}
                 disableDefaultUI={true}
                 >
-                <Marker  position={{lat: 40.000614, lng: -105.306558}} />
+                    { routeData.map((marker, index) => (
+                        <Marker  key={index} position={{lat: marker.latitude, lng: marker.longitude}} 
+                            onClick={() => setSelectedMarker(marker)}
+                        />
+                    ))}
+                    {selectedMarker && (
+                    <InfoWindow
+                        position={{lat: selectedMarker.latitude, lng: selectedMarker.longitude}}
+                        onCloseClick={() => setSelectedMarker(null)}
+                    >
+                        <h2> Route: {selectedMarker.name} </h2>
+                    </InfoWindow>
+                    )}
                 </Map>
             </APIProvider>
         </Flex>
