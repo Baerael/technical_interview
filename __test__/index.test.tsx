481089021e9d05af88f384ef1e79f05db2b68075
Baerael <badartic@yahoo.com>
2024-09-20 11:08:52 -0600

    finally got this to work!!!!!!!!!!!!!!!

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -1,9 +1,17 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
+import ThemeContextProvider from "../src/app/contexts/themeContext";
 import Page from "../src/app/page";
 
 
-test("Page", () => {
-  render(<Page />);
-  expect(screen.getByRole('heading', { level: 1, name: 'hello world' })).toBeDefined()
+describe("Page", () => {
+  it('heading has text hello world', () => {
+    render(<ThemeContextProvider><Page /></ThemeContextProvider>);
+    expect(screen.getByText(/hello world/)).toBeDefined()
+  })
+  it('<h1> exists', () => {
+    render(<ThemeContextProvider><Page /></ThemeContextProvider>);
+    expect(screen.getByRole('heading', { level: 1, name: 'hello world' })).toBeDefined()
+  })
+
 });
