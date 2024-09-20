9d117c0db7437d86c1b2e17d5d01a366a5edcbb2
Baerael <badartic@yahoo.com>
2024-09-20 11:42:39 -0600

    context types

--- src/app/contexts/themeContext.tsx
+++ src/app/contexts/themeContext.tsx
@@ -1,16 +1,20 @@
 "use client";
-import { createContext, useContext, useState } from "react";
-
-interface ThemeContextType {
-  theme: string;
-  setTheme: (theme: string) => void;
-}
+import { createContext, useContext, useState, ReactNode, Dispatch, SetStateAction } from "react";
 
 type AccentColor = "indigo" | "plum";
 
+interface ThemeContextType {
+  theme: AccentColor; // Changed from 'string' to 'AccentColor'
+  setTheme: Dispatch<SetStateAction<AccentColor>>; // This now matches the useState setter type
+}
+
+interface ThemeContextProviderProps {
+  children: ReactNode;
+}
+
 export const ThemeContext = createContext<ThemeContextType | null>(null);
 
-export default function ThemeContextProvider({ children }) {
+export default function ThemeContextProvider({ children }: ThemeContextProviderProps) {
   const [theme, setTheme] = useState<AccentColor>("indigo");
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
